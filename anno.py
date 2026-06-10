839efd0d8933ef80369ad55b5f70bdc6ef76a895
sharjil <madkisharjil@gmail.com>
2026-06-11 02:09:49 +0530

    new changes

--- anno.py
+++ anno.py
@@ -5,3 +5,9 @@ while enter_pass != password:
     enter_pass = input("Wrong password please try again: ")
 
 print("Congratulations! You have entered the correct password.")
+
+  new
+
+After the merge, if there are no conflicts, Git will create a merge commit automatically.
+
+If you get any error or conflict, paste the exact output here and I'll help you fix it.
