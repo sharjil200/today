c49247ddd7370406b89b14c06a00edccefbd3d68
sharjil <madkisharjil@gmail.com>
2026-06-11 00:56:25 +0530

    logn commit

---
+++ anno2.py
@@ -0,0 +1,18 @@
+import random
+
+secret_number = random.randint(1, 10)
+
+while True:
+    guess = int(input("Guess a number between 1 and 10: "))
+
+    if guess == secret_number:
+        print("🎉 Correct! You win!")
+        break
+    elif guess < secret_number:
+        print("Too low! Try again.")
+    else:
+        print("Too high! Try again.")
+
+
+
+
